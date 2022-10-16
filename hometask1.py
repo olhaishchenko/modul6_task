e4067d774d500bce5cdb448efbe6ab09ba5971f1
olhaishchenko <olga.ua.olga@gmail.com>
2022-10-16 11:58:02 +0200

    add exists

--- hometask1.py
+++ hometask1.py
@@ -1,34 +1,41 @@
+from genericpath import exists
 from locale import normalize
 from os import remove
 from pathlib import Path
 from shutil import move
 begin = "/Users/olha/Downloads"
 p = Path(begin)
-# Path(begin+'/images').mkdir()  
-# Path(begin+'/documents').mkdir()
-# Path(begin+'/audio').mkdir()
-# Path(begin+'/video').mkdir()
-# Path(begin+'/archives').mkdir()
-# Path(begin+'/other').mkdir()
+if not Path(begin+'/images').exists():
+    Path(begin+'/images').mkdir()
+if not Path(begin+'/documents').exists():
+    Path(begin+'/documents').mkdir()
+if not Path(begin+'/audio').exists():
+    Path(begin+'/audio').mkdir()
+if not Path(begin+'/video').exists():
+    Path(begin+'/video').mkdir()
+if not Path(begin+'/archives').exists():
+    Path(begin+'/archives').mkdir()
+if not Path(begin+'/other').exists():
+    Path(begin+'/other').mkdir()
 
 
-def ordnung(begin):
+def sort(begin):
     for i in p.iterdir():
         if i.is_file():
+          
             if i.suffix.lower() in ('.jpeg', '.png', '.jpg', '.svg'):
                 move(i,Path(begin+'/images/'+i.name))
-                # print(Path(begin+'/images/'+i.name))
 
-            if i.suffix.lower() in ('.doc', '.docx', '.txt', '.pdf', '.xlsx', '.pptx'):
+            elif i.suffix.lower() in ('.doc', '.docx', '.txt', '.pdf', '.xlsx', '.pptx'):
                 move(i,Path(begin+'/documents/'+i.name))
 
-            if i.suffix.lower() in ('.mp3', '.ogg', '.wav', '.amr'):
+            elif i.suffix.lower() in ('.mp3', '.ogg', '.wav', '.amr'):
                 move(i,Path(begin+'/audio/'+i.name))
 
-            if i.suffix.lower() in ('.avi', '.mp4', '.mov', '.mkv'):
+            elif i.suffix.lower() in ('.avi', '.mp4', '.mov', '.mkv'):
                 move(i,begin+'/video/'+i.name)
     
-            if i.suffix.lower in ('.zip', '.gz', '.tar'):
+            elif i.suffix.lower() in ('.zip', '.gz', '.tar'):
                 move(i,begin+'/archives/'+i.name)
 
             else:
@@ -39,4 +46,4 @@ def ordnung(begin):
         # else: 
         #     # перевірити на пустоту і видалити????
         #     ordnung(i)
-ordnung(begin)
+sort(begin)
