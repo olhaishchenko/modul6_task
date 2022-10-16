ff50b104d1acd45ae703508ff8865908e99c8812
olhaishchenko <olga.ua.olga@gmail.com>
2022-10-16 14:03:34 +0200

    rmdir

--- hometask1.py
+++ hometask1.py
@@ -1,46 +1,68 @@
+#!/usr/bin/python3
 import os
+import sys
 from pathlib import Path
 from shutil import move
 
-begin = os.getcwd()
-print(begin)
+START_FOLDER = sys.argv[1] if len(sys.argv)>1 else "/Users/olha/Downloads/Test"
+print(START_FOLDER)
 
-if not Path(begin+'/images').exists():
-    Path(begin+'/images').mkdir()
-if not Path(begin+'/documents').exists():
-    Path(begin+'/documents').mkdir()
-if not Path(begin+'/audio').exists():
-    Path(begin+'/audio').mkdir()
-if not Path(begin+'/video').exists():
-    Path(begin+'/video').mkdir()
-if not Path(begin+'/archives').exists():
-    Path(begin+'/archives').mkdir()
-if not Path(begin+'/other').exists():
-    Path(begin+'/other').mkdir()
+# check START_FOLDER exist and a folder
+
+IMAGE_PATH = START_FOLDER+'/images'
+DOCUMENT_PATH = START_FOLDER+'/documents'
+
+if not Path(IMAGE_PATH).exists():
+    Path(IMAGE_PATH).mkdir()
+if not Path(DOCUMENT_PATH).exists():
+    Path(DOCUMENT_PATH).mkdir()
+if not Path(START_FOLDER+'/audio').exists():
+    Path(START_FOLDER+'/audio').mkdir()
+if not Path(START_FOLDER+'/video').exists():
+    Path(START_FOLDER+'/video').mkdir()
+if not Path(START_FOLDER+'/archives').exists():
+    Path(START_FOLDER+'/archives').mkdir()
+if not Path(START_FOLDER+'/other').exists():
+    Path(START_FOLDER+'/other').mkdir()
 
 
-def sort(begin,now_folder):
+def normalize_file(i):
+    # TODO
+    pass
+
+def sort(now_folder):
     p=Path(now_folder)
+
     for i in p.iterdir():
+        if i.is_dir():
+            if i.name not in ('images' ,'documents','audio','video','archives','other'):
+                sort(i)
+
         if i.is_file():
-          
+            normalize_file(i)
+
             if i.suffix.lower() in ('.jpeg', '.png', '.jpg', '.svg'):
-                move(i,Path(begin+'/images/'+i.name))
+                move(i,Path(IMAGE_PATH+'/'+i.name))
 
             elif i.suffix.lower() in ('.doc', '.docx', '.txt', '.pdf', '.xlsx', '.pptx'):
-                move(i,Path(begin+'/documents/'+i.name))
+                move(i,Path(DOCUMENT_PATH+'/'+i.name))
 
             elif i.suffix.lower() in ('.mp3', '.ogg', '.wav', '.amr'):
-                move(i,Path(begin+'/audio/'+i.name))
+                move(i,Path(START_FOLDER+'/audio/'+i.name))
 
             elif i.suffix.lower() in ('.avi', '.mp4', '.mov', '.mkv'):
-                move(i,begin+'/video/'+i.name)
+                move(i,START_FOLDER+'/video/'+i.name)
     
             elif i.suffix.lower() in ('.zip', '.gz', '.tar'):
-                move(i,begin+'/archives/'+i.name)
+                move(i,START_FOLDER+'/archives/'+i.name)
 
             else:
-                move(i,begin+'/other/'+i.name)
-        if not i.is_file():
-            if i.name not in ('images' ,'documents','audio','archives','other'):
-                sort(begin,i)
+                move(i,START_FOLDER+'/other/'+i.name)
+        
+        if i.is_dir():
+            if i.name not in ('images' ,'documents','audio','video','archives','other'):
+                Path(i).rmdir()
+        
+
+if __name__ == "__main__":
+    sort(START_FOLDER)
